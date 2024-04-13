288ca7a51755eeca87fd8c96a1003bdaf7f1dd22
Icro Mateos <icaromatheus741@gmail.com>
2024-04-12 22:58:46 -0300

    terceira comissao

---
+++ Model/Menu.js
@@ -0,0 +1,8 @@
+function menu() {
+    var opcoes = document.getElementById("Menu");
+    if (opcoes.style.display === "none") {
+        opcoes.style.display = "block";
+    } else {
+        opcoes.style.display = "none";
+    }
+}
